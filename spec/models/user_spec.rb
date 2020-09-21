c4438bf07ec045fa235a6b8e8410ab74bd2a2a97
masafumiog <gurasum041219@gmail.com>
2020-09-22 04:10:16 +0900

    コード見直し（１回目）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
+
+    it "すべての情報が正しいフォーマットで入力されてていればユーザー登録できる"　do
+      @user = FactoryBot.build(:user)
+      expect(@user).to be_vaild
+    end
+
     it "nicknameが空だと登録できない" do
       @user = FactoryBot.build(:user)  # Userのインスタンス生成
       @user.nickname = ""
@@ -49,6 +55,64 @@ RSpec.describe User, type: :model do
       @user.birthday = ""
       @user.valid?
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
+    end
+
+    it "メールアドレスが登録済みであるとユーザー登録できない" do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has aleady been taken")
+    end
+
+    it "メールアドレスに@が含まれていないとユーザー登録できない" do
+    @user = FactoryBot.build(:user) 
+    expect(another_user.errors.full_messages).to include("Email has aleady been taken")
+  end
+
+  it "パスワードが5文字以下だとユーザー登録できない" do
+    @user = FactoryBot.build(:user) 
+    @user.password = "a1234"
+    expect(another_user.errors.full_messages).to include("Password is too long (minimum is 6 characters)")
+  end
+  it "パスワードが英字のみだとユーザー登録できない" do
+  @user = FactoryBot.build(:user) 
+  @user.password = "asdfgh"
+  expect(@user.errors.full_messages).to include("Password can't be blank")
+  end
+
+  it = "パスワードが数字のみだとユーザー登録できない" do
+  @user = FactoryBot.build(:user) 
+  @user.password = "123456"
+  expect(@user.errors.full_messages).to include("Password can't be blank")
+  end
+
+    it "姓が全角（漢字・ひらがな・カタカナ）以外だとユーザー登録できない" do
+  @user = FactoryBot.build(:user) 
+  @user.name_sei = "tanaka"
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Name sei can't be blank")
+  end
+
+ it "名が全角（漢字・ひらがな・カタカナ）以外だとユーザー登録できない" do
+  @user = FactoryBot.build(:user) 
+  @user.name_mai = "taro"
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Name sei can't be blank")
+  end
+
+  it "姓のフリガナが全角（カタカナ）以外だとユーザー登録できない"
+  @user = FactoryBot.build(:user) 
+  @user.kana_sei = "たなか"
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Kana sei can't be blank")
+  end
+
+it "名のフリガナが全角（カタカナ）以外だとユーザー登録でき" do
+  @user = FactoryBot.build(:user) 
+      @user.kana_mei = "たろう"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Kana sei can't be blank")
     end
   end
 end
