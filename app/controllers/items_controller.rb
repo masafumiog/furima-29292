e5abc52f3d8884d5f5e5b58c7a298b5051789155
masafumiog <gurasum041219@gmail.com>
2020-09-29 19:09:43 +0900

    コード見直し（２回目）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,9 +40,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-      @item = Item.find(params[:id])
-      @item.destroy
+     if @item.destroy
       redirect_to root_path
+     else
+      render :show
+     end
   end
  
   private
