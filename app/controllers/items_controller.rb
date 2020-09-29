7ea50167550a180b2ea91b0dc65b912e7fa01ff0
masafumiog <gurasum041219@gmail.com>
2020-09-29 18:31:52 +0900

    コード見直し（１回目）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :create]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except:[:index]
  
   def index
