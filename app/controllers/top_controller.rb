7bb509118d4009f6e8038425bd370171fe0bc7d9
masafumiog <gurasum041219@gmail.com>
2020-10-06 11:37:21 +0900

    コード見直し（３回目）

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,27 @@
+class TopController < ApplicationController
+#   before_action :move_to_index, except: [:index, :show]
+
+
+#   def index
+#     @items = Item.order("created_at DESC")
+#   end
+
+#   # def edit
+#   #   @tweet = Item.find(params[:id])
+#   # end
+
+
+#   # def show
+#   #   @item = Item.find(params[:id])
+#   # end
+  
+#   private
+
+#   def move_to_index
+#     unless user_signed_in?
+#       redirect_to action: :index
+#     end
+#   end
+
+
+end
